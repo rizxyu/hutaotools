34af9860ccedd4e662a92cc57bd9bd197fc2fece
mininxd <mininsna@gmail.com>
2025-01-21 16:50:27 +0700

    improve theme changer

--- public/js/mode.js
+++ public/js/mode.js
@@ -4,14 +4,37 @@ const toggle = document.querySelector('.theme-controller');
 
 toggle.addEventListener('change', () => {
     localStorage.setItem('theme', toggle.checked ? toggle.value : '');
-    const modeText = document.getElementById("switch-mode-text");
-    modeText.textContent = (modeText.textContent === 'Mode Terang') ? 'Mode Gelap' : 'Mode Terang';
+    navbar.classList.toggle("dark-theme");
+    navbar.classList.toggle("shadow-xl", "rounded-full");
+    document.querySelectorAll(".nav-btn").forEach(btn => {
+  const currentColor = getComputedStyle(btn).color;
+  const textMode = document.getElementById("switch-mode-text");
+  btn.style.color = currentColor === "rgb(29, 35, 42)" ? "#ffffff" : "#1d232a"; 
+  if(currentColor == "rgb(29, 35, 42)") {
+    textMode.textContent = "Mode Gelap"
+  } else {
+    textMode.textContent = "Mode Terang"
+  }
+});
 });
 
 // Load themes
   const savedTheme = localStorage.getItem('theme');
+
   if (savedTheme) {
     toggle.checked = true;
-    document.getElementById("switch-mode-text").textContent = "Mode Terang";
-    };
+    navbar.classList.toggle("dark-theme");
+    document.querySelectorAll(".nav-btn").forEach(btn => {
+      btn.style.color = "#1d232a";
+      
+      const currentColor = getComputedStyle(btn).color;
+      const textMode = document.getElementById("switch-mode-text");
+      if(currentColor == "rgb(29, 35, 42)") {
+       textMode.textContent = "Mode Gelap"
+      } else {
+       textMode.textContent = "Mode Terang"
+  }
+    })
+}
+
 });
